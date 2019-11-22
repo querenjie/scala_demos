5d377ae868340f6d11734917f89d14d7d96a75e3
querenjie <347794892@qq.com>
2019-11-22 11:45:53 +0800

    add some comments in it.

--- src/main/scala/demo/classtest/ImplicitClassDemo.scala
+++ src/main/scala/demo/classtest/ImplicitClassDemo.scala
@@ -1,21 +1,31 @@
 package demo.classtest
 
 object ImplicitClassDemo {
-  case class Rectangle(width:Int, height:Int) {
-    var x = width
-    var y = height
+  case class Rectangle(width1:Int, height1:Int) {
+    var x = width1
+    var y = height1
 
     def calcArea(): Int = {
       return x * y
     }
   }
+
+  /**
+    * 此处定义一个隐式类，在类名后面要跟上入参。
+    * 在使用的时候是用不上类名的，但是要用到入参。入参是什么类型的实参也必须是什么类型的。
+    * 隐式类中自己定义方法，方法也有入参。
+    * 使用的时候直接可以这样用：隐式类的实参 方法名 方法实参
+    * @param width
+    */
   implicit class RectangleMarker(width: Int) {
-    def x(height: Int) = Rectangle(width, height)
+    //此处的方法体定义为返回Rectangle对象。
+    def 乘(height: Int) = Rectangle(width, height)
   }
 
   def main(args: Array[String]): Unit = {
-    val easyRec = 3 x 4
+    //隐式类的使用，返回一个Rectangle对象
+    val easyRec = 3 乘 4
     println(easyRec.calcArea())
-    println((3 x 4).calcArea())
+    println((5 乘 2).calcArea())
   }
 }
--- src/main/scala/demo/classtest/ImplicitClassDemo2.scala
+++ src/main/scala/demo/classtest/ImplicitClassDemo2.scala
@@ -1,20 +1,38 @@
 package demo.classtest
 
 object ImplicitClassDemo2 {
+
+  /**
+    * 定义隐式类。
+    * 隐式类在使用的过程中无需显示创建类的对象就能使用其中的方法。
+    * 在这个例子中可以找到隐式类的使用方式：在隐式类定义中有入参和对应的类型，在类体中有方法名为times，方法的入参貌似是任意类型的对象。
+    * 于是在使用的时候可以用这样的形式来使用： 入参 times 任意对象
+    * @param x
+    */
   implicit class IntTimes(x: Int) {
-    def times[B](f1: =>B) {
+    var y = 0;
+    //方法times的具体功能是循环调用f1对象x次，f1的类型T猜想应该可以是任意类型。
+    def times[T](f1: => T) {
       def loop(current: Int): Unit ={
         if(current > 0){
           f1
+          y = y + 1
           loop(current - 1)
         }
       }
       loop(x)
+      println("调用了" + y + "次")
     }
 
   }
 
+  def a(i: Int):Unit = {
+    print(i+1)
+  }
+
   def main(args: Array[String]): Unit = {
+    //在使用的时候，times方法的入参传入的是一个方法
     5 times println("hello")
+    3 times a(3)
   }
 }
