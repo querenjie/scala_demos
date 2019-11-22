a45a207ef4a24684ff34a28293ac6543e3a94c09
querenjie <347794892@qq.com>
2019-11-22 16:35:26 +0800

    add some comments in it. Committed by Roger.Que on Nov 22, 2019.

--- src/main/scala/demo/forif/ForIfDemo.scala
+++ src/main/scala/demo/forif/ForIfDemo.scala
@@ -1,6 +1,11 @@
 package demo.forif
 
 object ForIfDemo {
+  /**
+    * 将Vector中的大于5的偶数添加到新的Vector中并返回。
+    * @param v
+    * @return
+    */
   def evenGT5(v:Vector[Int]):Vector[Int] = {
     var result = Vector[Int]()
     for {
@@ -12,12 +17,17 @@ object ForIfDemo {
     result
   }
 
+  /**
+    * 将Vector中小于10的所有奇数都添加到新的vector中并返回。
+    * @param v
+    * @return
+    */
   def yielding(v: Vector[Int]):Vector[Int] = {
     val result = for {
       n <- v
       if n < 10
       if n % 2 != 0
-    } yield n
+    } yield n       //for循环语句后面跟上yield n会自动把n记录到Vector中最后返回Vector。
     return result
   }
 
