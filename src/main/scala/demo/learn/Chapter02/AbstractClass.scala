6929ac3aac3fa8bb7694cb142fb312a07b7bb73a
querenjie <347794892@qq.com>
2019-11-22 17:09:47 +0800

    Committed by Roger.Que on Nov 22, 2019.

--- src/main/scala/demo/learn/Chapter02/AbstractClass.scala
+++ src/main/scala/demo/learn/Chapter02/AbstractClass.scala
@@ -1,11 +1,17 @@
 package demo.learn.Chapter02
 
+/**
+  * 定义一个抽象类，其中有一个变量和2个方法声明
+  */
 abstract class MyWriter {
   var message: String = "null"
   def setMessage(message: String):Unit
   def printMessage():Unit
 }
 
+/**
+  * 定义一个具体类实现抽象类中的方法。
+  */
 class ConsolePrinter extends MyWriter {
   def setMessage(contents: String):Unit= {
     this.message = contents
@@ -16,10 +22,10 @@ class ConsolePrinter extends MyWriter {
   }
 }
 
-trait lowerCase extends MyWriter {
-  abstract override def setMessage(contents: String) : Unit= 
-    printMessage()
-}
+//trait lowerCase extends MyWriter {
+//  abstract override def setMessage(contents: String) : Unit=
+//    printMessage()
+//}
 
 object AbstractClass {
   def main(args: Array[String]): Unit = {
