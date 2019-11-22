05969dd3466861d385e284db6e7b6701eee81a12
querenjie <347794892@qq.com>
2019-11-22 19:16:17 +0800

    Committed by Roger.Que on Nov 22, 2019.

--- src/main/scala/demo/list/ListDemo.scala
+++ src/main/scala/demo/list/ListDemo.scala
@@ -2,6 +2,8 @@ package demo.list
 
 object ListDemo {
   def main(args: Array[String]): Unit = {
+    var mul0 = List.tabulate(5)(_ + 0)
+    println("mul0 : " + mul0)
     var mul1 = List.tabulate( 4 , 5 )( _ - _ )
     println( "mul1 : " + mul1  )
     var mul2 = List.tabulate( 4 , 5 )( _ + _ )
--- src/main/scala/demo/list/Others1.scala
+++ src/main/scala/demo/list/Others1.scala
@@ -7,7 +7,7 @@ object Others1 {
     及先对元素1和元素2进行处理，将结果与元素3进行处理，以此类推，
     即为reduce
      */
-    val a = (1 to 4).reduceLeft(_*_)        //等同于1*2*3*4*5*6*7*8*9
+    val a = (1 to 4).reduceLeft(_*_)        //等同于1*2*3*4
     println("a=" + a)
   }
 }
