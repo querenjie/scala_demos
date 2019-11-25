fdd698f70bb8e5f4ee09fefd7f2fc3db2207d538
querenjie <347794892@qq.com>
2019-11-25 16:02:00 +0800

    Committed by Roger.Que on Nov 25, 2019.

--- src/main/scala/demo/map_and_flatmap/MapAndFlatMapDemo.scala
+++ src/main/scala/demo/map_and_flatmap/MapAndFlatMapDemo.scala
@@ -12,9 +12,15 @@ object MapAndFlatMapDemo {
     println()
     //foreach对传入的每一个元素都进行处理，但是没有返回值
     (1 to 9 ).map("@"*_).foreach(println(_))
-    //filter:对传入的每一个元素都进行条件判断，如果对元素返回true，则保留该元素，否则过滤掉该元素
+    //filter:对传入的每一个元素都进行条件判断，如果返回true，则保留该元素，否则过滤掉该元素
     val rdd1 = (1 to 20 ).filter(_%2==0)
     rdd1.foreach(x=>{val y=x+"\t";print(y)})
     println()
+
+    /**flatMap是map的一种扩展。在flatMap中，我们会传入一个函数，该函数对每个输入都会返回一个集合（而不是一个元素），然后，flatMap把生成的多个集合“拍扁”成为一个集合。*/
+    val books = List("Hadoop","Hive","HDFS")
+    val charList = books.flatMap(elem=>elem.toList)
+    println(charList)
+    charList.foreach(print(_))
   }
 }
